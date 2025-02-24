ba9cc5f32243331f14f0b298f8d102fa5a6509c5
nono20794 <chardon@post.bgu.ac.il>
2025-02-24 16:57:39 +0200

    add troop a

--- src/components/pages/ATroop.jsx
+++ src/components/pages/ATroop.jsx
@@ -2,11 +2,13 @@ import Tabs from "../Tabs";
 import "./Atroop.css";
 export default function ATroop() {
   return (
-    <section className="troop-container">
+    <section className="troop-container atroop-container">
       <h1>סוללה א</h1>
       <Tabs>
         <div className="section-1" title="שמירות">
-          <h1>this is section 1</h1>
+          <div className="div1">1</div>
+          <div className="div1">2</div>
+          <div className="div1">3</div>
         </div>
         <div className="section-2" title="עמדות">
           <h1>this is section 2</h1>
